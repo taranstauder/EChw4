956a91bfdcc0a57030cf6ebe05afc8bc6a62bc57
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-10-05 13:42:22 -0500

    Update design-top.php

--- includes/design-top.php
+++ includes/design-top.php
@@ -1,3 +1,3 @@
-<div style="background-color: purple; font-family: monospace;" class="jumbotron">
+<div style="background-color: #4466cc; font-family: monospace;" class="jumbotron">
 	<h1>E-Commerce 4013: Homework 4</h1>
 </div>
