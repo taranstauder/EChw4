5e443b7ab19b58482d51536b8edd595825666b1e
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-10-12 13:59:41 -0500

    Update instructor.php

--- instructor.php
+++ instructor.php
@@ -9,16 +9,8 @@
   <body>
     <?php include("includes/design-top.php");?>
     <?php include("includes/navigation.php");?>
-    <h1>Instructors</h1>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Office Number</th>
-    </tr>
-  </thead>
-  <tbody>
+    <div class="container">
+    
     <?php
 $servername = "localhost";
 $username = "tstauouc_suser";
@@ -31,19 +23,93 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  switch ($_POST['saveType']) {
+    case 'Add':
+    $sqlAdd = "insert into instructor (instructor_name) value (?)";
+    $stmtAdd = $conn->prepare($sqlAdd);
+    $stmtAdd->bind_param("s", $_POST[iName']);
+    $stmtAdd->execute();
+    echo '<div class="alert alert-success" role="alert">New instructor added.</div>':
+    break;
+    case 'Edit':
+    $sqlEdit = "update instructor set instructor_name=? where instructor_id=?";
+    $stmtEdit = $conn->prepare($sqlEdit);
+    $stmtEdit->bind_param("si", $_POST[iName'], $_POST['iid']);
+    $stmtEdit->execute();
+    echo '<div class="alert alert-success" role="alert">Edited Instructor.</div>':
+    break;
+    case 'Delete':
+    $sqlDelete = "delete from instructor where instructor_id=?";
+    $stmtDelete = $conn->prepare($sqlDelete);
+    $stmtDelete->bind_param("i", $_POST[iid']);
+    $stmtDelete->execute();
+    echo '<div class="alert alert-success" role="alert">New instructor added.</div>':
+    break;
+  }
+}    
+?>
+    <h1>Instructors</h1>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th></th>
+          <th></th>
+         </tr>
+        </thead>
+        </tbody>
+        
+<?php
 
-$sql = "SELECT instructor_id, instructor_name, instructor_office from instructor";
+$sql = "SELECT instructor_id, instructor_name from instructor";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-  <tr>
-    <td><?=$row["instructor_id"]?></td>
-    <td><a href="course_instructor.php?id=<?=$row["instructor_id"]?>"><?=$row["instructor_name"]?></a></td>
-    <td><?=$row["instructor_office"]?></td>
-  </tr>
+          
+          <tr>
+            <td><?=$row["instructor_id"]?></td>
+            <td><a href="instructors_course.php?id=<?=$row["instructor_id"]?>"><?=$row["instructor_name"]?></a></td>
+            <td>
+              <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editInstructor<?=$row["instructor_id"]?>">
+                Edit
+              </button>
+              <div class="modal fade" id="editInstructor<?=$row["instructor_id"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editInstructor<?=$row["instructor_id"]?>Label" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="editInstructor<?=$row["instructor_id"]?>Label">Edit Instructor</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <form method="post" action="">
+                        <div class="mb-3">
+                          <label for="editInstructor<?=$row["instructor_id"]?>Name" class="form-label">Name</label>
+                          <input type="text" class="form-control" id="editInstructor<?=$row["instructor_id"]?>Name" aria-describedby="editInstructor<?=$row["instructor_id"]?>Help" name="iName" value="<?=$row['instructor_name']?>">
+                          <div id="editInstructor<?=$row["instructor_id"]?>Help" class="form-text">Enter the instructor's name.</div>
+                        </div>
+                        <input type="hidden" name="iid" value="<?=$row['instructor_id']?>">
+                        <input type="hidden" name="saveType" value="Edit">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <form method="post" action="">
+                <input type="hidden" name="iid" value="<?=$row["instructor_id"]?>" />
+                <input type="hidden" name="saveType" value="Delete">
+                <button type="submit" class="btn" onclick="return confirm('Are you sure?')">Delete</button>
+              </form>
+            </td>
+          </tr>
+          
 <?php
   }
 } else {
@@ -51,8 +117,38 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-  </tbody>
-    </table>
+          
+        </tbody>
+      </table>
+      <br />
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addInstructor">
+        Add New
+      </button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="addInstructor" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addInstructorLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="addInstructorLabel">Add Instructor</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form method="post" action="">
+                <div class="mb-3">
+                  <label for="instructorName" class="form-label">Name</label>
+                  <input type="text" class="form-control" id="instructorName" aria-describedby="nameHelp" name="iName">
+                  <div id="nameHelp" class="form-text">Enter the instructor's name.</div>
+                </div>
+                <input type="hidden" name="saveType" value="Add">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
