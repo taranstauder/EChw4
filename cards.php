b624e6df5b26745492869a6d9ad32981f8a3951c
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-10-19 17:41:57 -0500

    Update cards.php

--- cards.php
+++ cards.php
@@ -3,18 +3,17 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Enrollments</title>
+    <title>Enrollment</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <?php include("includes/design-top.php");?>
-    <?php include("includes/navigation.php");?>
-    <h1>Enrollment</h1>
-    <table class="table table-striped">
+    <h1>Enrollments</h1>
+<table class="table table-striped">
   <thead>
     <tr>
       <th>Name</th>
-      <th>Course</th>
+      <th>Description</th>
+      <th></th>
     </tr>
   </thead>
   <tbody>
@@ -22,7 +21,7 @@
 $servername = "localhost";
 $username = "tstauouc_suser";
 $password = "{kmXl,4Kf[Ea";
-$dbname = "tstauouc_hw4";
+$dbname = "tstauouc_sample";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -30,16 +29,16 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-  
-$sql = "SELECT * from enrollment e join student s on s.student_id = e.student_id join course c on c.course_id = e.course_id;
-$result = $conn->query($sql);   
+
+$sql = "select student_name, c.description from enrollment e join student s on s.student_id = e.student_id join course c on c.course_id = e.course_id";
+$result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
   <tr>
-    <td><?=$row["student_name"]?></td>
+    <td><?=$row["student_id"]?></td>
     <td><?=$row["description"]?></td>
     <td>
       <form method="post" action="cards-edit.php">
@@ -55,8 +54,8 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-  </card-group>
-
+  </tbody>
+    </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
