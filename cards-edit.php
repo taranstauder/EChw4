0cfaed204f44f5b4764126989b7c3b67f35db270
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-10-19 16:46:01 -0500

    Create cards-edit.php

---
+++ cards-edit.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Edit Cards</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Edit Cards</h1>
+<?php
+$servername = "localhost";
+$username = "tstauouc_suser";
+$password = "{kmXl,4Kf[Ea";
+$dbname = "tstauouc_hw4";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * from enrollment where enrollment_id=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $_POST['id']);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+<form method="post" action="card-edit-save.php">
+  <div class="mb-3">
+    <label for="eNumber" class="form-label">Enrollment number</label>
+    <input type="text" class="form-control" id="eNumber" aria-describedby="nameHelp" name="eNumber" value="<?=$row['enrollment_id']?>">
+    <div id="nameHelp" class="form-text">Enter the enrollment number.</div>
+  </div>
+  <div class="mb-3">
+  <label for="courseList" class="form-label">Courses</label>
+<select class="form-select" aria-label="Select course" id="course_id" name="cid">
+<?php
+    $courseSql = "select * from course order by description";
+    $courseResult = $conn->query($courseSql);
+    while($courseRow = $courseResult->fetch_assoc()) {
+      if ($courseRow['course_id'] == $row['course_id']) {
+        $selText = " selected";
+      } else {
+        $selText = "";
+      }
+?>
+  <option value="<?=$courseRow['course_id']?>"<?=$selText?>><?=$courseRow['description']?></option>
+<?php
+    }
+?>
+</select>
+  </div>
+  <input type="hidden" name="id" value="<?=$row['enrollment_id']?>">
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
