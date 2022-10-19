72082f656458eeea048e1b53260f210f101f1d21
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-10-19 16:26:50 -0500

    Update navigation.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -15,5 +15,8 @@
 	<li class="nav-item">
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Student") {?>active<?php }?>" href="student.php">Student</a>
 	  </li>	
+	<li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Cards") {?>active<?php }?>" href="cards.php">Cards</a>
+	  </li>	
 	</ul>
 </div>
