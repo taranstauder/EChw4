cbfca2a82f4f96dd4dd75f0a14607700a33e324b
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-10-19 14:06:32 -0500

    Update navigation.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -10,7 +10,7 @@
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Instructor") {?>active<?php }?>" href="instructor.php">Instructors</a>
 	  </li>
 	<li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Enrollment") {?>active<?php }?>" href="enroll.php">Take</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Enrollment") {?>active<?php }?>" href="enroll.php">Enrollment</a>
 	  </li>
 	<li class="nav-item">
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Student") {?>active<?php }?>" href="student.php">Student</a>
