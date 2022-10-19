32139566409d78b68fb786f645fb2357d7add884
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-10-19 16:28:50 -0500

    Create cards.php

---
+++ cards.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Enrollments</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php include("includes/design-top.php");?>
+    <?php include("includes/navigation.php");?>
+    <h1>Enrollment</h1>
+<div class="card-group">
+    <?php
+$servername = "localhost";
+$username = "tstauouc_suser";
+$password = "{kmXl,4Kf[Ea";
+$dbname = "tstauouc_hw4";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+  
+$sql = "SELECT student_id, student_name from student";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+   <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?=$row["student_name"]?></h5>
+      <p class="card-text"><ul>
+<?php      
+    $section_sql = "select c.description from take t join student s on s.student_id = t.student_id join course c on c.course_id = t.course_id where s.student_id=" . $row["student_id"];
+    $section_result = $conn->query($section_sql);
+    
+    while($section_row = $section_result->fetch_assoc()) {
+      echo "<li>" . $section_row["description"] . "</li>";
+    }
+?>
+      </ul></p>
+  </div>
+    </div>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </card-group>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
