278046261daf5713101e1fe7338988a805d56ebf
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-10-24 14:13:28 -0500

    Update cards.php

--- cards.php
+++ cards.php
@@ -3,18 +3,18 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>course</title>
+    <title>Enrollment</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
         <?php include("includes/design-top.php");?>
     <?php include("includes/navigation.php");?>
-    <h1>courses</h1>
+    <h1>Enrollments</h1>
 <table class="table table-striped">
   <thead>
     <tr>
+      <th>Name</th>
       <th>Description</th>
-      <th>Instructor</th>
       <th></th>
     </tr>
   </thead>
@@ -32,7 +32,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "select course_id, description, i.instructor_name, c.prefix, c.number, i.instructor_office from course c join instructor i on i.instructor_id = c.instructor_id";
+$sql = "select student_name, c.description from enrollment e join student s on s.student_id = e.student_id join course c on c.course_id = e.course_id";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -40,11 +40,11 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
 ?>
   <tr>
+    <td><?=$row["student_name"]?></td>
     <td><?=$row["description"]?></td>
-    <td><?=$row["instructor_name"]?></td>
     <td>
       <form method="post" action="cards-edit.php">
-        <input type="hidden" name="id" value="<?=$row["course_id"]?>">
+        <input type="hidden" name="id" value="<?=$row["enrollment_id"]?>">
         <input type="submit" value="Edit">
       </form>
     </td>
