2ced0a921cc35d04e7818b31f46916da88e950dd
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-10-24 14:43:16 -0500

    Create cards-edit-save.php

---
+++ cards-edit-save.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Edit Enroll</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    
+<?php
+$servername = "localhost";
+$username = "tstauouc_suser";
+$password = "{kmXl,4Kf[Ea";
+$dbname = "tstauouc_hw4";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$iName = $_POST['student_id'];
+
+$sql = "update enrollment set course_id=? where student_id=?";
+//echo $sql;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("si", $student_id, $_POST['iid']);
+    $stmt->execute();
+?>
+    
+    <h1>Edit </h1>
+<div class="alert alert-success" role="alert">
+  edited.
+</div>
+    <a href="cards.php" class="btn btn-primary">Go back</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
