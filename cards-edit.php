4cb9454290d060cad4c93a3cf6eca1f8a05bbd5f
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-10-24 14:19:37 -0500

    Update cards-edit.php

--- cards-edit.php
+++ cards-edit.php
@@ -22,7 +22,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT * from enrollment where enrollment_id=?";
+$sql = "SELECT * from enrollment where student_name=?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $_POST['id']);
 $stmt->execute();
