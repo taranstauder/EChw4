bcd456ac03bcb1e78556671c2c45d90b1b76e51d
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-10-26 12:08:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <?php include("includes/design-top.php");?>
     <?php include("includes/navigation.php");?>  
     <h1>Hello!</h1>
+    <p>Course, Instructors, Enrollment and Student are tabs with the information from their respective tables. On these pages you can add, edit, and delete records and pop ups and notifications occur. The drop tab allows you to edit joined records with a drop down feature.</p>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
